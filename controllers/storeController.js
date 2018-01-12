374f8195491f2e4d87b5daa32436a8ce2cacc060
Dariusz awariusz <dariusz.hebda@bluepaprica.com>
2018-01-12 17:18:02 +0100

    saving data in mongodb + using Async and Await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index')
@@ -8,8 +11,13 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
     //response json request body
     // console.log(req.body);
-    res.json(req.body);
+    // res.json(req.body);
+
+    const store = new Store(req.body);
+    // mongo save data or show error
+    await store.save()
+    res.redirect('/');
 };
