f6ffc665629aa2cddf0a2da65ef1e2d931350e00
Dariusz awariusz <dariusz.hebda@bluepaprica.com>
2018-01-16 08:52:58 +0100

    Routing and templating single stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -80,3 +80,10 @@ exports.updateStore = async (req, res) => {
     res.redirect(`/stores/${store._id}/edit`);
     // Redirect them the store and teel them it worked
 }
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  // res.json(store)
+  if(!store) return next();
+  res.render('store', { store, title: store.name });
+}
