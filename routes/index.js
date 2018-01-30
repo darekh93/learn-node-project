44675fae81d812a4cea4011d744448b3a77d8aaf
Dariusz awariusz <dariusz.hebda@bluepaprica.com>
2018-01-30 14:14:32 +0100

    creating geo ajax endpoits and api

--- routes/index.js
+++ routes/index.js
@@ -53,5 +53,6 @@ router.post('/add/:id',
   // API
 
   router.get('/api/search', catchErrors(storeController.searchStores));
+  router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
